bc2332cfb3e4f06348731f1a12817bd405ec7150
Michael Dadzie <dadziemikke2@gmail.com>
2023-10-01 20:00:36 +0000

    Initial Commit

---
+++ jotter/jotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  jotterApp.swift
+//  jotter
+//
+//  Created by Michael Dadzie on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct jotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

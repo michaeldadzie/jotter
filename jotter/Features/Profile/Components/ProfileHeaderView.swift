2e5d27aa16073f8b0f901248003b3a6e4f038cd4
Michael Dadzie <dadziemikke2@gmail.com>
2023-10-30 13:37:56 +0000

    added profile component

---
+++ jotter/Features/Profile/Components/ProfileHeaderView.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+struct ProfileHeaderView: View {
+    let user: User?
+    
+    init(user: User?) {
+        self.user = user
+    }
+    
+    var body: some View {
+        HStack(alignment: .top) {
+            VStack(alignment: .leading, spacing: 12) {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(user?.fullname ?? "")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    Text(user?.username ?? "")
+                        .font(.subheadline)
+                }
+                
+                if let bio = user?.bio {
+                    Text(bio)
+                        .font(.footnote)
+                }
+                
+                Text("2 followers")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+            
+            Spacer()
+            
+            CircularProfileImageView()
+        }
+    }
+}
+
+struct ProfileHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHeaderView(user: dev.user)
+    }
+}

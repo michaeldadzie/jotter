2f75f3deddf8fdf5077a7201aad2c49e5106977c
Michael Dadzie <dadziemikke2@gmail.com>
2023-10-30 13:36:36 +0000

    added explore view and view model

---
+++ jotter/Features/Explore/View/ExploreView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct ExploreView: View {
+    @StateObject var viewModel = ExploreViewModel()
+    @State private var search = ""
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(viewModel.users) { user in
+                        NavigationLink(value: user) {
+                            VStack {
+                                UserCell(user: user)
+                                Divider()
+                            }
+                        }
+                        .padding(.vertical, 4)
+                    }
+                }
+            }
+            .navigationDestination(for: User.self, destination: { user in
+                UserProfileView(user: user)
+            })
+            .navigationTitle("Search")
+            .searchable(text: $search, prompt: "Search")
+        }
+    }
+}
+
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreView()
+    }
+}
---
+++ jotter/Features/Explore/ViewModel/ExploreViewModel.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+class ExploreViewModel: ObservableObject {
+    @Published var users = [User]()
+    
+    init() {
+        Task { try await fetchUsers() }
+    }
+    
+    @MainActor
+    private func fetchUsers() async throws {
+        self.users = try await UserService.fetchUsers()
+    }
+}

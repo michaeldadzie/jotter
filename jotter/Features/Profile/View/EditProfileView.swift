80bce11c7956c416d20006dee2151c732fc589c7
Michael Dadzie <dadziemikke2@gmail.com>
2023-10-30 13:38:46 +0000

    added edit profile view

---
+++ jotter/Features/Profile/View/EditProfileView.swift
@@ -0,0 +1,81 @@
+import SwiftUI
+
+struct EditProfileView: View {
+    @State private var bio = ""
+    @State private var link = ""
+    @State private var isPrivateProfile = false
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color(.systemGroupedBackground)
+                    .edgesIgnoringSafeArea([.bottom, .horizontal])
+                
+                VStack {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Name")
+                                .fontWeight(.semibold)
+                            Text("Michael Dadzie")
+                        }
+                        
+                        Spacer()
+                        
+                        CircularProfileImageView()
+                    }
+                    
+                    Divider()
+                    
+                    VStack(alignment: .leading) {
+                        Text("Bio")
+                            .fontWeight(.semibold)
+                        TextField("Edit your bio...", text: $bio, axis: .vertical)
+                    }
+                   
+                    Divider()
+                    
+                    VStack(alignment: .leading) {
+                        Text("Link")
+                            .fontWeight(.semibold)
+                        TextField("Add link...", text: $link)
+                    }
+                    
+                    Divider()
+                    
+                    Toggle("Private profile", isOn: $isPrivateProfile)
+                }
+                .font(.footnote)
+                .padding()
+                .background(.white)
+                .cornerRadius(10)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(.systemGray4), lineWidth: 1)
+                }
+                .padding()
+            }
+            .navigationTitle("Edit Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {}
+                        .font(.subheadline)
+                        .foregroundColor(.black)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {}
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.black)
+                }
+            }
+        }
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}

cbfb0547d26af105ed7d36fe486cf3a46263c48d
Michael Dadzie <dadziemikke2@gmail.com>
2023-10-30 13:35:57 +0000

    added tab view

---
+++ jotter/Core/TabBar/JotterTabView.swift
@@ -0,0 +1,56 @@
+import SwiftUI
+
+struct JotterTabView: View {
+    @State private var selectedTab = 0
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            FeedView()
+                .tabItem {
+                    Image(systemName: selectedTab == 0 ? "house.fill" : "house")
+                        .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                }
+                .onAppear { selectedTab = 0 }
+                .tag(0)
+            
+            ExploreView()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+                .onAppear { selectedTab = 1 }
+                .tag(1)
+            
+            Text("Post")
+                .tabItem {
+                    Image(systemName: selectedTab == 2 ? "plus.app.fill" : "plus.app")
+                        .environment(\.symbolVariants, selectedTab == 2 ? .fill : .none)
+                }
+                .onAppear { selectedTab = 2 }
+                .tag(2)
+            
+            Text("Activity")
+                .tabItem {
+                    Image(systemName: selectedTab == 3 ? "heart.fill" : "heart")
+                        .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
+                }
+                .onAppear { selectedTab = 3 }
+                .tag(3)
+            
+            ProfileView()
+                .tabItem {
+                    Image(systemName: selectedTab == 4 ? "person.fill" : "person")
+                        .environment(\.symbolVariants, selectedTab == 4 ? .fill : .none)
+                }
+                .onAppear { selectedTab = 4 }
+                .tag(4)
+            
+        }
+        .tint(.black)
+    }
+}
+
+struct JotterTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        JotterTabView()
+    }
+}

1b6f8a6ef710bd45de0162cd5ebfd532142ba296
Michael Dadzie <dadziemikke2@gmail.com>
2023-10-30 13:30:18 +0000

    added components

---
+++ jotter/Core/Components/CircularProfileImageView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct CircularProfileImageView: View {
+    var body: some View {
+        Image(systemName: "person")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 40, height: 40)
+            .clipShape(Circle())
+    }
+}
+
+struct CircularProfileImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularProfileImageView()
+    }
+}
---
+++ jotter/Features/Feed/View/ThreadCellView.swift
@@ -0,0 +1,73 @@
+import SwiftUI
+
+struct ThreadCellView: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 12) {
+                CircularProfileImageView()
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("michaeldadzie")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Spacer()
+                        
+                        Text("10m")
+                            .font(.caption)
+                            .foregroundColor(Color(.systemGray3))
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundColor(Color(.darkGray))
+                        }
+                    }
+                    
+                    Text("michael is an ios dev")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    
+                    HStack(spacing: 16) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "heart")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bubble.right")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrow.rectanglepath")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "paperplane")
+                        }
+                    }
+                    .foregroundColor(.black)
+                    .padding(.vertical, 8)
+                }
+            }
+            
+            Divider()
+        }
+        .padding()
+    }
+}
+
+struct ThreadCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThreadCellView()
+    }
+}

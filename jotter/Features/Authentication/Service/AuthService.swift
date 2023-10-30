f2f1025fbd631e0f2b5feeddc54e809ea18fd7e6
Michael Dadzie <dadziemikke2@gmail.com>
2023-10-30 13:36:06 +0000

    added auth service

---
+++ jotter/Features/Authentication/Service/AuthService.swift
@@ -0,0 +1,59 @@
+import Firebase
+import FirebaseFirestoreSwift
+
+
+class AuthService {
+    @Published var userSession: FirebaseAuth.User?
+    
+    static let shared = AuthService()
+    
+    init() {
+        self.userSession = Auth.auth().currentUser
+    }
+    
+    @MainActor
+    func login(withEmail email: String, password: String) async throws {
+        do {
+            let result = try await Auth.auth().signIn(withEmail: email, password: password)
+            self.userSession = result.user
+            try await UserService.shared.fetchCurrentUser()
+        } catch {
+            print("DEBUG: Failed to log user in with error \(error.localizedDescription)")
+        }
+    }
+    
+    @MainActor
+    func signup(
+        withEmail email: String,
+        password: String,
+        fullname: String,
+        username: String
+    ) async throws {
+        do {
+            let result = try await Auth.auth().createUser(withEmail: email, password: password)
+            self.userSession = result.user
+            try await uploadUserData(withEmail: email, fullname: fullname, username: username , id: result.user.uid)
+        } catch {
+            print("DEBUG: Failed to create user with error \(error.localizedDescription)")
+        }
+    }
+    
+    @MainActor
+    func uploadUserData(
+        withEmail email: String,
+        fullname: String,
+        username: String,
+        id: String
+    ) async throws {
+        let user = User(id: id, fullname: fullname, email: email, username: username)
+        guard let userData = try? Firestore.Encoder().encode(user) else { return}
+        try await Firestore.firestore().collection("users").document(id).setData(userData)
+        UserService.shared.currentUser = user
+    }
+    
+     func signOut() {
+        try? Auth.auth().signOut()
+        self.userSession = nil
+         UserService.shared.reset()
+    }
+}

0477b44a1f4cf167c67f580c710b9eafa80624e8
Michael Dadzie <dadziemikke2@gmail.com>
2023-10-30 13:36:14 +0000

    added user service

---
+++ jotter/Core/Service/UserService.swift
@@ -0,0 +1,32 @@
+import Firebase
+import FirebaseFirestoreSwift
+
+class UserService {
+    @Published var currentUser: User?
+    
+    static let shared = UserService()
+    
+    init() {
+        Task { try await fetchCurrentUser() }
+    }
+    
+    @MainActor
+    func fetchCurrentUser() async throws {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let snapshot = try await Firestore.firestore().collection("users").document(uid).getDocument()
+        let user = try snapshot.data(as: User.self)
+        self.currentUser = user
+    }
+    
+    static func fetchUsers() async throws -> [User] {
+        guard let currentUID = Auth.auth().currentUser?.uid else { return [] }
+        let snapshot = try await Firestore.firestore().collection("users").getDocuments()
+        let users = snapshot.documents.compactMap({ try? $0.data(as: User.self) })
+        return users.filter({ $0.id != currentUID })
+    }
+    
+    func reset() {
+        self.currentUser = nil
+    }
+}
+

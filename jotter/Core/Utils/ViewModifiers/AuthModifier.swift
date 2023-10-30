61ab26fc67ebf6a8fcc153fe639b5bc231ccc669
Michael Dadzie <dadziemikke2@gmail.com>
2023-10-30 13:34:37 +0000

    extracted auth modifiers

---
+++ jotter/Core/Utils/ViewModifiers/AuthModifier.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct AuthTextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .padding(12)
+            .background(Color(.systemGray6))
+            .cornerRadius(10)
+            .padding(.horizontal, 24)
+    }
+}
+
+struct AuthButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .fontWeight(.semibold)
+            .foregroundColor(.white)
+            .frame(width: 352, height: 44)
+            .background(.black)
+            .cornerRadius(8)
+            
+    }
+}

155408ea305417d3366bd4d0355b8c54e469247b
Michael Dadzie <dadziemikke2@gmail.com>
2023-10-30 13:35:45 +0000

    refactored content view

---
+++ jotter/Core/Root/View/ContentView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var viewModel = ContentViewModel()
+    
+    var body: some View {
+        Group {
+            if viewModel.userSession != nil {
+                JotterTabView()
+            } else {
+                LoginView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

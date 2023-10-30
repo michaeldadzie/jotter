35d6e0be4a620ed79ec77a7f8fe0a581d28eea4d
Michael Dadzie <dadziemikke2@gmail.com>
2023-10-30 13:38:30 +0000

    added model for custom tab view

---
+++ jotter/Features/Profile/Model/ProfileThreadFilter.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+enum ProfileThreadFilter: Int, CaseIterable, Identifiable {
+case threads
+case replies
+    
+    var title: String {
+        switch self {
+        case .threads: return "Threads"
+        case .replies: return "Replies"
+        }
+    }
+    
+    var id: Int { return self.rawValue }
+}
